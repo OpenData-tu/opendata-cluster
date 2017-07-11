180b1c51a5c8aa171cd5e9796f15e3c1b77d9fc8
Amer <amer.jazaerli@gmail.com>
2017-07-11 19:43:54 +0200

    Fixes a bug. Really wait for Zookeeper to become ready.

--- scripts/wait_zookeeper.sh
+++ scripts/wait_zookeeper.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 source ./scripts/colors.sh
-source ./config/kafka_topics
 
 echo -n "Waiting fot Zookeeper to become ready." 
-until kubectl get pods --namespace=kafka 2> /dev/null | grep "zoo" | wc -l | xargs | grep -q "5"
+until kubectl get pods --namespace=kafka 2> /dev/null | grep "zoo" | grep "1/1" | wc -l | xargs | grep -q "5"
 do
   printf "."
   sleep 1
