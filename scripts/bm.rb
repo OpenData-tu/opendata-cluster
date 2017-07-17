c9608d26c5c5b6904125093c59f24da02126a287
Amer <amer.jazaerli@gmail.com>
2017-07-18 00:41:35 +0200

    Get rid of benchmark shell script

--- scripts/bm.rb
+++ scripts/bm.rb
@@ -1,19 +1,29 @@
 #!/bin/ruby
 require "time"
+require 'open-uri'
+require 'json'
+
+# http://yourelasticsearch.com:9200/data-loadtest/_count
 
 
-def get_count
-  %x["./scripts/bm.sh"].to_i
+def get(url)
+  open(url) do |f|
+    page_string = f.read
+  end
+end
+
+def get_count(url)
+  JSON.parse(get(url))["count"].to_i
 end
 
 
-def run_bm(time_to_sleep)
-  count_start = get_count
+def run_bm(time_to_sleep,url)
+  count_start = get_count(url)
   puts "Count start: #{count_start}" 
 
   sleep time_to_sleep
 
-  count_end = get_count
+  count_end = get_count(url)
   puts "Count end: #{count_end}" 
 
   speed = count_end - count_start
@@ -21,14 +31,17 @@ def run_bm(time_to_sleep)
   return speed
 end
 
+time_to_sleep = 10
+
+url = ARGV[0]
 tests = []
 10.times do
-  tests.push(run_bm(10))
+  tests.push(run_bm(time_to_sleep,url))
 end
 
-def mean arr
-  arr.reduce(:+).to_f / arr.size
+def mean(arr,time_to_sleep)
+  (arr.reduce(:+).to_f / arr.size)/time_to_sleep
 end
 
 puts
-puts "#{mean tests}/10s"
+puts "#{mean(tests,time_to_sleep)}/s"
